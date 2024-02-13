c0fed6e39aad23fb56024b926eda2a13d71772d0
dsampath23 <159919675+dsampath23@users.noreply.github.com>
2024-02-13 22:04:05 +0530

    Create private-subnet.tf

---
+++ private-subnet.tf/private-subnet.tf
@@ -0,0 +1,27 @@
+######################################################################
+## Private Subnets
+###
+resource "aws_subnet" "private" { 
+	vpc_id                  = "${var.vpc_id}"
+	cidr_block        		= "${element(split(",", var.private_subnets), count.index)}"
+	availability_zone 		= "${element(split(",", var.azs), count.index)}"
+	count             		= "${length(compact(split(",", var.private_subnets)))}"
+	map_public_ip_on_launch = false
+	tags                    = {
+								"Name"				= "${element(split(",", var.subnet_name_tag), count.index)}"
+								"Application ID" 	= "${var.sub_application_id_tag }"
+								"Application Role"	= "${var.sub_application_role_tag}"
+								"Environment"		= "${var.environment_tag}"
+								"Owner"				= "${var.owner_tag}"
+								"Business Unit"		= "${var.business_unit_tag}"
+								"Financial"			= "${var.financial_tag}"
+								"Security"			= "${var.security_tag}"
+								"Automation"		= "${var.automation_tag}"
+                             }
+}
+
+resource "aws_route_table_association" "private-rtb" {
+	count = "${length(compact(split(",", var.private_subnets)))}"
+	subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
+	route_table_id = "${var.private_route_table_id}"
+}
