b24a4a07414ce711c8926f48295083f8e09c56da
dsampath23 <159919675+dsampath23@users.noreply.github.com>
2024-02-14 15:03:21 +0530

    Create output.tf

---
+++ aws-elb-web/output.tf
@@ -0,0 +1,14 @@
+######################################################################
+## Outputs
+###
+output "web_elb_name" {
+  value = "${aws_elb.elb.name}"
+}
+
+output "elb_id" {
+  value = "${aws_elb.elb.id}"
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.elb.dns_name}"
+}
