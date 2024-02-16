a11e1a2cc313dc4771ed7bc125356e788fa7cabc
dsampath23 <159919675+dsampath23@users.noreply.github.com>
2024-02-16 15:39:19 +0530

    Create variable.tf

---
+++ aws-webserver-sg/variable.tf
@@ -0,0 +1,15 @@
+#VPC
+variable "vpc_cidr" {}
+variable "vpc_id" {}
+
+#Tag
+variable "webuisg" {}
+variable "webapisg" {}
+variable owner_tag {}
+variable business_unit_tag {}
+variable environment_tag {}
+variable security_tag {}
+variable automation_tag {}
+variable financial_tag {}
+variable application_id_tag {}
+variable application_role_tag {}
