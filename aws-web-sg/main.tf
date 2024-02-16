1f21647244ebe27e52419c9451c150242517a605
dsampath23 <159919675+dsampath23@users.noreply.github.com>
2024-02-16 15:35:15 +0530

    Create main.tf

---
+++ aws-web-sg/main.tf
@@ -0,0 +1,71 @@
+// Security Group Resource for Module
+resource "aws_security_group" "main_security_group" {
+    name = "${var.security_group_name}"
+    description = "Security Group ${var.security_group_name}"
+    vpc_id = "${var.vpc_id}"
+
+    // allows traffic from the SG itself for tcp
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "tcp"
+        self = true
+    }
+
+    // allows traffic from the SG itself for udp
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "udp"
+        self = true
+    }
+
+    // allow traffic for TCP 22
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["${split(",", var.rdp_cidr_block)}"]
+    }
+
+    // allow traffic for TCP 80
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["${var.source_cidr_block}"]
+    }
+
+    // allow traffic for TCP 443
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["${var.source_cidr_block}"]
+    }
+
+    // allow traffic for TCP 8080
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["${var.source_cidr_block}"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    } 
+	
+	tags {
+		"Name" 				= "${var.security_group_name}"
+		"Environment" 		= "${var.environment_tag}"			
+  }	
+}
+
+// Output ID of sg_web SG we made
+output "security_group_id_web" {
+  value = "${aws_security_group.main_security_group.id}"
+}
