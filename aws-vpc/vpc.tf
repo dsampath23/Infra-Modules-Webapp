d934e782e2dee1cce6b9a4e3dd2be8554dfb980f
dsampath23 <159919675+dsampath23@users.noreply.github.com>
2024-02-21 13:06:46 +0530

    Update vpc.tf

--- aws-vpc/vpc.tf
+++ aws-vpc/vpc.tf
@@ -5,6 +5,7 @@ resource "aws_vpc" "vpc" {
     cidr_block           = "${var.vpc_cidr}"
     enable_dns_support   = true
     enable_dns_hostnames = true
+    enable_default_roots = true
     tags                 = {
 							                "Name"				= "${var.vpc_name_tag}"
 							                "Application ID" 	= "${var.vpc_application_id_tag }"
